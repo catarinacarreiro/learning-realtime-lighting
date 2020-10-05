200d1e2c0b055d4fc62f6d313dae316397ddf26f
Catarina <46010838+catarinacarreiro@users.noreply.github.com>
2020-10-05 13:27:16 +0100

    game can now end!

---
+++ Assets/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+
+}
